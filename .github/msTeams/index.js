fe930a8daa4f32dc3403d905aadc73269632f34b
Zacharias Pavlatos <zaharias.pavlatos@gmail.com>
2022-04-26 13:17:23 +0200

    asd

--- .github/msTeams/index.js
+++ .github/msTeams/index.js
@@ -1,19 +1,46 @@
 const core = require("@actions/core");
-
+const axios = require("axios");
 const msTeamsWebhook = core.getInput("ms-teams-webhook-uri", {
   required: true,
 });
 
-(async () => {
-  const rawResponse = await fetch(msTeamsWebhook, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+const msTeamsCard = {
+  "@type": "MessageCard",
+  "@context": "https: //schema.org/extensions",
+  themeColor: "0078D7",
+  summary: "notificationSummary",
+  title: "${{ env.commitMsg }}",
+  sections: [
+    {
+      activityTitle: "${{ env.authorName }}",
+      activityImage: "avatar_url",
+      activitySubtitle: "sdf",
     },
-    body: JSON.stringify({ a: 1, b: "Textual content" }),
-  });
-  const content = await rawResponse.json();
+  ],
+  potentialAction: [
+    {
+      "@context": "http: //schema.org",
+      target: "ssdfsdfs",
+      "@type": "ViewAction",
+      name: "View Workflow Run",
+    },
+    {
+      "@context": "http: //schema.org",
+      target: "[commit.data.html_url]",
+      "@type": "ViewAction",
+      name: "View Commit Changes",
+    },
+  ],
+};
 
-  console.log(content);
-})();
+const sendPostRequest = async (msTeamsCard) => {
+  try {
+    const response = await axios.post(msTeamsWebhook, msTeamsCard);
+    console.log(response);
+    core.debug(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+sendPostRequest();
